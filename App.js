8fc96558ac604f6703d2a7619b576c809aa48f97
Dmitrii Salenko <dmitrii.salenko@litslink.com>
2018-11-22 18:32:14 +0200

    Simple demo

--- App.js
+++ App.js
@@ -1,29 +1,92 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import SamsungHealth from 'rn-samsung-health';
+import get from 'lodash/get';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+  state = {
+    steps: null,
+    stepsError: null,
+    connected: false,
+    connectionResult: 'Not connected',
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  readSteps = () => {
+    SamsungHealth.getDailyStepCount({}, (error, result) => {
+      if (error) {
+        this.setState({
+          stepsError: error,
+        });
+      } else {
+        this.setState({
+          // -1 if no data
+          steps: get(result, '0.data.0.steps', -1),
+          stepsError: null,
+        });
+      }
+    });
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  handleConnect = () => {
+    SamsungHealth.authorize((error, result) => {
+      if (error) {
+        this.setState({
+          connected: false,
+          connectionResult: `Connection error: ${error}`,
+        });
+      } else {
+        this.setState({
+          connected: true,
+          connectionResult: 'Connected successfully',
+        });
+        this.readSteps();
+      }
+    });
+  }
+
+  handleDisconnect = () => {
+    SamsungHealth.stop();
+    this.setState({
+      connected: false,
+      connectionResult: 'Disconnected',
+    });
+  }
+
   render() {
+    const { connected, connectionResult, steps, stepsError } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {(steps === 0 || steps) &&
+          <Text style={styles.steps}>
+            {steps}
+          </Text>
+        }
+        {stepsError &&
+          <Text style={styles.stepsError}>
+            {stepsError}
+          </Text>
+        }
+        <Text style={styles.connectionStatusText}>
+          {connectionResult}
+        </Text>
+        {!connected &&
+          <View style={styles.button}>
+            <Button 
+              title="Connect Samsung Health"
+              onPress={this.handleConnect}
+            />
+          </View>
+        }
+        {connected &&
+          <View style={styles.button}>
+            <Button
+              title="Disconnect"
+              onPress={this.handleDisconnect}
+              style={styles.button}
+            />
+          </View>
+        }
       </View>
     );
   }
@@ -36,14 +99,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  button: {
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  connectionStatusText: {
+    margin: 10,
   },
+  steps: {
+    margin: 50,
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
 });
